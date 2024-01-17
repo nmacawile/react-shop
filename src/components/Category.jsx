971cea638cae90bd609c8dc051e00f4f071c740c
Normar Macawile <nmacawile@gmail.com>
2024-01-17 02:34:32 +0000

    Refactor Category component

--- src/components/Category.jsx
+++ src/components/Category.jsx
@@ -1,10 +1,12 @@
 import { useParams } from "react-router-dom";
-import titleCase from "../helpers/titleCase";
+import { categories } from "../data/categories";
 
 export function Category() {
   const { category } = useParams();
-
-  return <h1 className="text-2xl">{titleCase(category)}</h1>;
+  const title =
+    categories.find((c) => c.slug === category)?.name ||
+    "Category not available";
+  return <h1 className="text-2xl">{title}</h1>;
 }
 
 export default Category;
--- src/components/Category.test.jsx
+++ src/components/Category.test.jsx
@@ -4,18 +4,48 @@ import { render, screen } from "@testing-library/react";
 import Category from "./Category.jsx";
 
 describe("Category component test", () => {
-  it("renders the category title", () => {
-    const categories = ["clothes", "electronics", "shoes", "furniture", "miscellaneous"];
-    for (const category in categories) {
+  describe("when category is valid", () => {
+    it("renders the category title", () => {
+      const categories = [
+        { slug: "apparel-and-fashion", name: "Apparel and Fashion" },
+        { slug: "electronics", name: "Electronics" },
+        { slug: "home-and-furniture", name: "Home and Furniture" },
+        { slug: "health-and-beauty", name: "Health and Beauty" },
+        { slug: "sports-and-outdoors", name: "Sports and Outdoors" },
+        { slug: "toys-and-games", name: "Toys and Games" },
+      ];
+
+      categories.forEach((category) => {
+        render(
+          <>
+            <MemoryRouter initialEntries={[`/categories/${category.slug}`]}>
+              <Routes>
+                <Route path="/categories/:category" element={<Category />} />
+              </Routes>
+            </MemoryRouter>
+          </>
+        );
+
+        expect(
+          screen.getByText(RegExp(category.name, "i"))
+        ).toBeInTheDocument();
+      });
+    });
+  });
+
+  describe("when category is invalid", () => {
+    it("renders 'Category not available'", () => {
       render(
-        <MemoryRouter initialEntries={[`/categories/${category}`]}>
-          <Routes>
-            <Route path="/categories/:category" element={<Category />} />
-          </Routes>
-        </MemoryRouter>
+        <>
+          <MemoryRouter initialEntries={[`/categories/invalid-category`]}>
+            <Routes>
+              <Route path="/categories/:category" element={<Category />} />
+            </Routes>
+          </MemoryRouter>
+        </>
       );
 
-      expect(screen.getByText(RegExp(category, "i"))).toBeInTheDocument();
-    }
+      expect(screen.getByText("Category not available")).toBeInTheDocument();
+    });
   });
 });
