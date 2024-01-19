2fec864bd6d2dfe4f9af80beb0633476fcc41dad
Normar Macawile <nmacawile@gmail.com>
2024-01-19 04:46:41 +0000

    Change Search Form ring style

--- src/components/SearchForm.jsx
+++ src/components/SearchForm.jsx
@@ -27,7 +27,7 @@ const SearchForm = () => {
         <input
           type="search"
           id="default-search"
-          className="block w-64 sm:w-96 p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500"
+          className="block w-64 sm:w-96 p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-4 focus:ring-sky-500 focus:border-sky-500"
           placeholder="Search for products"
           required
         />
