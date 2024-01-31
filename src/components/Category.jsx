fdb0528a6e7fd82481237f8b7b57219f3c9011fc
Normar Macawile <nmacawile@gmail.com>
2024-01-31 16:09:41 +0000

    Show Product Card placeholders when page is not ready

--- src/components/Category.jsx
+++ src/components/Category.jsx
@@ -1,25 +1,39 @@
 import { useParams } from "react-router-dom";
+import { useState, useEffect } from "react";
 import { categories } from "../data/categories";
 import { ProductCard } from "./ProductCard.jsx";
 import { fakeData } from "../data/fakeData.js";
+import ProductCardPlaceholder from "./ProductCardPlaceholder.jsx";
 
 export function Category() {
   const { category } = useParams();
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    setLoading(false);
+  }, []);
+
   const title =
     categories.find((c) => c.slug === category)?.name ||
     "Category not available";
   return (
-    <section className="category">
-      <h1 className="text-2xl mb-8 text-left font-semibold text-gray-900">
-        {title}
-      </h1>
+    <>
+      <section className="category">
+        <h1 className="text-2xl mb-8 text-left font-semibold text-gray-900">
+          {title}
+        </h1>
 
-      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4">
-        {fakeData.map((product, i) => (
-          <ProductCard key={`product-${i}`} product={product} />
-        ))}
-      </div>
-    </section>
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4">
+          {loading
+            ? [1, 2, 3, 4, 5, 6, 7, 8].map((a) => (
+                <ProductCardPlaceholder key={`card-placeholder-${a}`} />
+              ))
+            : fakeData.map((product, i) => (
+                <ProductCard key={`product-${i}`} product={product} />
+              ))}
+        </div>
+      </section>
+    </>
   );
 }
 
---
+++ src/components/ProductCardPlaceholder.jsx
@@ -0,0 +1,18 @@
+export function ProductCardPlaceholder() {
+  return (
+    <div className="rounded-lg border border-gray-300 overflow-hidden">
+      <div className="p-8 w-full p-0 flex">
+        <div className="w-full h-full pb-[100%] rounded-md bg-gray-300 animate-pulse" />
+      </div>
+
+      <div className="flex flex-col px-6 pb-4 gap-3 items-start justify-center">
+        <div className="h-7 w-[10rem] rounded-md bg-gray-300 animate-pulse"></div>
+        <div className="h-5 w-[20rem] max-w-full rounded-md bg-gray-300 animate-pulse"></div>
+        <div className="h-4 w-[7rem] rounded-md bg-gray-300 animate-pulse"></div>
+        <div className="self-end h-10 w-[6rem] mt-2 rounded-md bg-gray-300 animate-pulse"></div>
+      </div>
+    </div>
+  );
+}
+
+export default ProductCardPlaceholder;
