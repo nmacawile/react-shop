e998506f95aca0294a628b387c0c4afdf3e299fe
Normar Macawile <nmacawile@gmail.com>
2024-02-10 02:14:33 +0000

    Increase bottom margin of the Breadcrumbs

--- src/components/Breadcrumbs.jsx
+++ src/components/Breadcrumbs.jsx
@@ -6,11 +6,11 @@ export function Breadcrumbs() {
   const breadcrumbs = useSelector((state) => state.breadcrumbs.value);
 
   return (
-    <div className="overflow-hidden w-full">
-      <nav className="overflow-hidden bg-gray-100 w-full text-ellipsis whitespace-nowrap flex flex-row gap-2 p-2 px-4 mb-4 text-md font-semibold text-gray-600 rounded justify-items-start w-full">
+    <div className="overflow-hidden w-full mb-4">
+      <nav className="overflow-hidden bg-gray-100 w-full text-ellipsis whitespace-nowrap flex flex-row gap-2 p-2 px-4 mb-4 text-md font-semibold text-gray-600 rounded justify-items-start w-full shadow">
         {breadcrumbs.map((breadcrumb, i) => {
           return (
-            <span key={`breadcrumb-${i}`} className="crumb">
+            <span key={`breadcrumb-${i}`} className="crumb text-ellipsis last:overflow-hidden last:text-ellipsis">
               {breadcrumb.link ? (
                 <Link className="text-sky-600" to={breadcrumb.link}>
                   {breadcrumb.title}
