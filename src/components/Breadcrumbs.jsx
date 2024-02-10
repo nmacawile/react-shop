77083115d9dc9bce0f90df2d99ace44688e9d1e0
Normar Macawile <nmacawile@gmail.com>
2024-02-10 01:59:11 +0000

    Refactor Breadcrumbs to render separators using CSS

--- src/components/Breadcrumbs.jsx
+++ src/components/Breadcrumbs.jsx
@@ -1,16 +1,16 @@
 import { Link } from "react-router-dom";
 import { useSelector } from "react-redux";
+import "./Breadcrumbs.css";
 
 export function Breadcrumbs() {
   const breadcrumbs = useSelector((state) => state.breadcrumbs.value);
-  const lastCrumb = (i) => breadcrumbs.length - 1 == i;
 
   return (
     <div className="overflow-hidden w-full">
       <nav className="overflow-hidden bg-gray-100 w-full text-ellipsis whitespace-nowrap flex flex-row gap-2 p-2 px-4 mb-4 text-md font-semibold text-gray-600 rounded justify-items-start w-full">
         {breadcrumbs.map((breadcrumb, i) => {
           return (
-            <span key={`breadcrumb-${i}`}>
+            <span key={`breadcrumb-${i}`} className="crumb">
               {breadcrumb.link ? (
                 <Link className="text-sky-600" to={breadcrumb.link}>
                   {breadcrumb.title}
@@ -18,7 +18,6 @@ export function Breadcrumbs() {
               ) : (
                 breadcrumb.title
               )}
-              {!lastCrumb(i) && " / "}
             </span>
           );
         })}
